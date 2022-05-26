9d64d1af4871de908e5faf5d51f09e1dde5eeee5
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-05-26 14:02:13 +0530

    [Debashis] Check Anagram Word

---
+++ AnagramPreoject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnagramPreoject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Anagram a = new Anagram();
+            Console.WriteLine("Find anagram word.");
+            Console.Write("Please enter first word: ");
+            string first = Console.ReadLine();
+            Console.Write("Please enter second word: ");
+            string second = Console.ReadLine();
+            if (a.IsAnagram(first, second))
+            {
+                Console.WriteLine("Given words are anagram");
+            }
+            else
+            {
+                Console.WriteLine("Not anagram, put another word.");
+            }
+        }
+    }
+}
